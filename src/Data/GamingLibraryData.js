e879dbdbe5a5d2067e3d79e7d2dfd32a6ff47b9e
Amir-Battal <ameerbatttal@hotmail.com>
2023-07-20 18:57:11 +0300

    Training-Projects

---
+++ src/Data/GamingLibraryData.js
@@ -0,0 +1,12 @@
+import Gaminglibrary_01 from '../assets/images/game-01.jpg';
+import Gaminglibrary_02 from '../assets/images/game-02.jpg';
+import Gaminglibrary_03 from '../assets/images/game-03.jpg';
+
+
+const GamingLibraryData = [
+    {id: "library_item_1", image: Gaminglibrary_01, title:"Fortnite"  ,     category:"Sandbox"  , date_added:"24/08/2036",    hours_played:"634  H 22 Mins",  downloaded:"Downloaded"  },
+    {id: "library_item_2", image: Gaminglibrary_02, title:"Pubg"      ,     category:"Stream-X" , date_added:"22/06/2036",    hours_played:"740  H 52 Mins",  downloaded:"Download"  },
+    {id: "library_item_3", image: Gaminglibrary_03, title:"Dota2"     ,     category:"Legendary", date_added:"21/04/2036",    hours_played:"892  H 14 Mins",  downloaded:"Downloaded"  }
+] 
+
+export default GamingLibraryData;
